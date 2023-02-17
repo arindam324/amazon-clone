e08ae5ee30c9895e2ae912c0892c2d5ba35bd3f8
arindam234 <aroy92647@gmal.com>
2023-02-17 18:40:08 +0530

    fixed hydration issue

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,30 +1,38 @@
-import React, {lazy, Suspense, ReactNode} from "react";
+import React, { lazy, Suspense, ReactNode, useState, useEffect } from "react";
 
 import Header from "../components/Header";
-import Footer from '../components/Footer'
+import Footer from "../components/Footer";
 
-import {useMedia} from 'react-use'
+import { useMedia } from "react-use";
 
-const DesktopHeader = lazy(() => import('./DesktopHeader'))
+const DesktopHeader = lazy(() => import("./DesktopHeader"));
 
 // import DesktopHeader from "./DesktopHeader";
 
-const Layout: React.FC<{ children: ReactNode }> = ({children}) => {
+const Layout: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const [islarge, setLarge] = useState<boolean>(true);
+  const isLarge = useMedia("(min-width:1080px)");
 
-    const isLarge = useMedia("(min-width:1080px)")
+  useEffect(() => {
+    if (isLarge) {
+      setLarge(true);
+    } else {
+      setLarge(false);
+    }
+  }, []);
 
-    console.log(isLarge)
+  console.log(isLarge);
 
-    return (
-        <main className={"relative pb-72 min-h-screen"}>
-            <Suspense fallback={null}>
-                {isLarge ? <DesktopHeader/> : <Header/>}
-            </Suspense>
+  return (
+    <main className={"relative pb-72 min-h-screen"}>
+      <Suspense fallback={null}>
+        {islarge ? <DesktopHeader /> : <Header />}
+      </Suspense>
 
-            {children}
-            <Footer/>
-        </main>
-    );
+      {children}
+      <Footer />
+    </main>
+  );
 };
 
 export default Layout;
--- pages/cart.tsx
+++ pages/cart.tsx
@@ -1,34 +1,56 @@
-import {useContext} from 'react'
-import Layout from '../components/Layout'
-import {ShoppingCartContext} from "../context/ShoppingCardContext";
-import Image from 'next/image'
+import { useContext } from "react";
+import Layout from "../components/Layout";
+import { XMarkIcon } from "@heroicons/react/24/outline";
+import { ShoppingCartContext } from "../context/ShoppingCardContext";
+import Image from "next/image";
 
 const Cart = () => {
-    const {items} = useContext(ShoppingCartContext)
-    return (
-        <div className={"w-full min-h-screen"}>
-            <Layout>
-                <div className={"mt-4 lg:mt-0 w-full"}>
-                    {items.map(item => (
-                        <div className={"flex flex-col lg:flex-row pt-12 mx-auto lg:space-x-8 lg:justify-center max-w-6xl  items-center p-4"}
-                             key={item.item.id}>
-                            <div className={"relative  w-20 h-20 lg:h-[600px] lg:w-[600px] "}>
-                                <Image src={item.item.image} className={"object-contain "} fill alt={""}/>
-                            </div>
-                            <div>
-                                <p className={"text-sm lg:text-2xl w-full lg:max-w-[70%]"}>{item.item.description}</p>
-                                <div className={"w-full mt-2 flex items-center space-x-4"}>
-                                    <p className={"text-xl font-semibold"}>{`₹${item.item.price}`}</p>
-                                    <p>Quality:<span className={"font-semibold"}> {item.quantity}</span></p>
-                                </div>
-                            </div>
-                        </div>
-                    ))}
+  const { items, removeItem } = useContext(ShoppingCartContext);
+  return (
+    <div className={"w-full min-h-screen"}>
+      <Layout>
+        <div className={"mt-4 lg:mt-0 w-full"}>
+          {items.map((item) => (
+            <div
+              className={
+                "flex flex-col lg:flex-row pt-12 mx-auto lg:space-x-8 lg:justify-center max-w-6xl  items-center p-4"
+              }
+              key={item.item.id}
+            >
+              <div className={"relative  w-20 h-20 lg:h-[600px] lg:w-[600px] "}>
+                <Image
+                  src={item.item.image}
+                  className={"object-contain "}
+                  fill
+                  alt={""}
+                />
+              </div>
+              <div>
+                <p className={"text-sm flex-1 lg:text-2xl w-full "}>
+                  {item.item.description}
+                </p>
+                <div className={"w-full mt-2 flex items-center space-x-4"}>
+                  <p
+                    className={"text-xl font-semibold"}
+                  >{`₹${item.item.price}`}</p>
+                  <p>
+                    Quality:
+                    <span className={"font-semibold"}> {item.quantity}</span>
+                  </p>
                 </div>
-            </Layout>
+              </div>
+              <div
+                onClick={() => removeItem(item.item.id)}
+                className="cursor-pointer"
+              >
+                <XMarkIcon className="lg:h-10 lg:w-10" />
+              </div>
+            </div>
+          ))}
         </div>
-    )
-}
+      </Layout>
+    </div>
+  );
+};
 
-
-export default Cart
+export default Cart;
